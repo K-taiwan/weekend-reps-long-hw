8daf83c56a13d9c5ee318bfd3721ab87e4263392
KennyK-TW <b2khakashu@yahoo.com>
2019-09-21 17:57:49 -0700

    1 down

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// . Q + A
+// How do we assign a value to a variable?
+making a unique name and equal to that value
+
+// How do we change the value of a variable?
+variable = value;
+
+// How do we assign an existing variable to a new variable?
+variable = newValue;
+// Remind me, what are declare, assign, and define?
+declare is register a variable like (let uniqueName);
+assign (let uniqueName = "Sam";)
+
+// What is pseudocoding and why should you do it?
+writing in your own words how the code should be executed step by step
+
+// What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+majority of the time should be spent on solving how to write the code before starting to type it
+
