816104d92e330157a2d4c119ac272721327546df
KennyK-TW <b2khakashu@yahoo.com>
2019-09-21 18:14:58 -0700

    B. Strings

--- js/app.js
+++ js/app.js
@@ -17,3 +17,34 @@ writing in your own words how the code should be executed step by step
 // What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
 majority of the time should be spent on solving how to write the code before starting to type it
 
+// B. Strings
+// Create a variable called firstVariable.
+// Assign it the value of the string "Hello World"
+// Change the value of this variable to some number.
+// Store the value of firstVariable in a new variable called secondVariable
+// Change the value of secondVariable to any string.
+var firstVariable;
+firstVariable = "Hello World";
+console.log(firstVariable);
+firstVariable = 9;
+console.log(firstVariable);
+var secondVariable = firstVariable;
+console.log(secondVariable);
+secondVariable = secondVariable.toString();
+console.log(typeof secondVariable);
+
+
+// What is the value of firstVariable?
+firstVariable = 9;
+console.log(firstVariable);
+
+right now it is equal to 9
+
+// Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
+
+var yourName = "Kenny";
+console.log(typeof yourName);
+
+yourName = "Hello, my name is " + "Kenny";
+console.log(yourName);
+
