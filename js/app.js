0611748376af71e194b3a9ec1d4ebc5d7bacf314
KennyK-TW <b2khakashu@yahoo.com>
2019-09-22 20:20:08 -0700

    B. printCool

--- js/app.js
+++ js/app.js
@@ -434,3 +434,15 @@ printGreeting("Slimer");
 // => Hello there, Slimer!
 
 // You think you could? I think so too. Feel free to skip this problem, because you've already done it. If you've done the problem twice, read entire problems carefully before doing them from now on.
+
+// B. printCool
+// Write a function printCool that accepts one parameter, name as an argument. The function should print the name and a message saying that that person is cool.
+
+// console.log(printCool("Captain Reynolds"));
+// => "Captain Reynolds is cool";
+
+const printCool = (string) => {
+    console.log(`${string} is cool`);
+    
+}
+printCool("Captain Reynolds");
