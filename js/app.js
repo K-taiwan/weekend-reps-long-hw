1201008501fef25f86ca713a59c2863fd76e1341
KennyK-TW <b2khakashu@yahoo.com>
2019-09-22 17:28:35 -0700

    G. Monkey in the Middle

--- js/app.js
+++ js/app.js
@@ -312,4 +312,23 @@ const testNum = (digit) => {
     }
 }
 testNum(99);
-testNum(101);
+// testNum(101);
+
+// G. Monkey in the Middle
+// Write an if ... else if ... else statement:
+// console.log() little number if the number entered is less than 5.
+// If the number entered is more than 10, log big number.
+// Otherwise, log "monkey".
+const monkey = (num) => {
+    if(num < 5){
+        console.log("little number");
+    } else if(num > 10) {
+        console.log("big number");
+    } else{
+        console.log("monkey");
+    }
+}
+monkey(1);
+monkey(5);
+monkey(9);
+monkey(99);
